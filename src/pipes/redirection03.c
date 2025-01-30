39333a291ab7847f2a7aad9086b35ec6ef4e0ad1
iviseras <ignacio.viseras@gmail.com>
2025-01-30 19:08:48 +0100

    clean code????

--- src/pipes/redirection03.c
+++ src/pipes/redirection03.c
@@ -1,31 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   redirection02.c                                    :+:      :+:    :+:   */
+/*   redirection03.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: igvisera <igvisera@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/12 15:58:28 by igvisera          #+#    #+#             */
-/*   Updated: 2025/01/30 15:15:07 by igvisera         ###   ########.fr       */
+/*   Created: 2025/01/30 15:14:40 by igvisera          #+#    #+#             */
+/*   Updated: 2025/01/30 15:17:28 by igvisera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-int	open_append_file(t_redirect_file *outfile)
+int	open_output_file(t_redirect_file *outfile)
 {
 	int	fd;
 
-	fd = open(outfile->value, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	fd = open(outfile->value, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd < 0)
 	{
-		perror("open append");
+		perror("open output");
 		exit(EXIT_FAILURE);
 	}
 	return (fd);
 }
 
-void	redirect_to_append_files(t_token *data)
+void	redirect_to_files(t_token *data)
 {
 	int				fd;
 	t_redirect_file	*outfile;
@@ -36,21 +36,20 @@ void	redirect_to_append_files(t_token *data)
 	while (outfiles)
 	{
 		outfile = (t_redirect_file *)outfiles->content;
-		fd = open_append_file(outfile);
+		fd = open_output_file(outfile);
 		if (outfiles->next)
 			close(fd);
 		outfiles = outfiles->next;
 	}
 	if (dup2(fd, STDOUT_FILENO) < 0)
 	{
-		perror("dup2 append");
-		close(fd);
+		perror("dup2 output");
 		exit(EXIT_FAILURE);
 	}
 	close(fd);
 }
 
-void	redirect_append(t_token *data, t_ast *ast, t_params *p, t_env *env)
+void	redirect_output(t_token *data, t_ast *ast, t_params *p, t_env *env)
 {
 	int	original_stdout;
 
@@ -60,15 +59,15 @@ void	redirect_append(t_token *data, t_ast *ast, t_params *p, t_env *env)
 		perror("dup original stdout");
 		exit(EXIT_FAILURE);
 	}
-	redirect_to_append_files(data);
+	redirect_to_files(data);
 	execute_node(ast, p, env);
 	restore_stdout(original_stdout);
 }
 
-void	init_redritect_append(t_ast *ast, t_params *p, t_env *env)
+void	init_redirct_out(t_ast *ast, t_params *p, t_env *env)
 {
 	t_token	*token;
 
 	token = (t_token *)(ast->data);
-	redirect_append(token, ast, p, env);
+	redirect_output(token, ast, p, env);
 }
