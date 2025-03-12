4a653bea03f9f3ce31feefc75b2307edeb0da664
drestrep <davidro112014@gmail.com>
2025-03-12 16:56:15 +0100

    Fixed STDINT and STDOUT dups for non-built-ins.

--- src/pipes/execute_cmd01.c
+++ src/pipes/execute_cmd01.c
@@ -6,7 +6,7 @@
 /*   By: drestrep <drestrep@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 09:32:52 by igvisera          #+#    #+#             */
-/*   Updated: 2025/03/12 16:30:20 by drestrep         ###   ########.fr       */
+/*   Updated: 2025/03/12 16:51:29 by drestrep         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,25 @@ void	before_execute(t_ast *node, t_params *p, t_env *env)
 
 	error = 0;
 	data = (t_token *)(node->data);
-	copy_stdin = dup(STDIN_FILENO);
-	copy_stdout = dup(STDOUT_FILENO);
-	close(STDIN_FILENO);
-	close(STDOUT_FILENO);
+	if (!is_builtin(data->cmd))
+	{
+		copy_stdin = dup(STDIN_FILENO);
+		copy_stdout = dup(STDOUT_FILENO);
+		close(STDIN_FILENO);
+		close(STDOUT_FILENO);
+	}
 	have_redirect = have_redirection(data);
 	if (have_redirect != -1)
 		error = handle_redirection(node);
 	if (error == 0)
 		execute_node(node, p, env);
-	close(STDIN_FILENO);
-	close(STDOUT_FILENO);
-	dup2(copy_stdout, STDOUT_FILENO);
-	dup2(copy_stdin, STDIN_FILENO);
+	if (!is_builtin(data->cmd))
+	{
+		close(STDIN_FILENO);
+		close(STDOUT_FILENO);
+		dup2(copy_stdout, STDOUT_FILENO);
+		dup2(copy_stdin, STDIN_FILENO);
+	}
 }
 
 void	pipes_and_execute(t_ast *node, t_params *p, t_env *env, t_token *data)
