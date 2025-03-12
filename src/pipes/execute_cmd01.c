674e1f6390e24ad5fd2aa5504c3e255f680ab5d0
drestrep <davidro112014@gmail.com>
2025-03-12 17:32:27 +0100

    Fixed built-ins.

--- src/pipes/execute_cmd01.c
+++ src/pipes/execute_cmd01.c
@@ -6,7 +6,7 @@
 /*   By: drestrep <drestrep@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 09:32:52 by igvisera          #+#    #+#             */
-/*   Updated: 2025/03/12 17:03:18 by drestrep         ###   ########.fr       */
+/*   Updated: 2025/03/12 17:27:39 by drestrep         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,14 +41,18 @@ void	before_execute(t_ast *node, t_params *p, t_env *env)
 
 	error = 0;
 	data = (t_token *)(node->data);
-	if (!is_builtin(data->cmd))
-		fd_dup(0);
 	have_redirect = have_redirection(data);
 	if (have_redirect != -1)
+	{
+		//if (ft_strcmp(data->cmd, "echo") == 0 || ft_strcmp(data->cmd, "cat") == 0)
+		if (ft_strcmp(data->cmd, "ls"))
+			fd_dup(0);
 		error = handle_redirection(node);
+	}
 	if (error == 0)
 		execute_node(node, p, env);
-	if (!is_builtin(data->cmd))
+	//if (have_redirect != -1 && (ft_strcmp(data->cmd, "echo") == 0 || ft_strcmp(data->cmd, "cat") == 0))
+	if (have_redirect != -1 && ft_strcmp(data->cmd, "ls"))
 		fd_dup(1);
 }
 
